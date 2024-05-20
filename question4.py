2770d257033ab7a95fcae06fbab54bfd956a78a2
Om prakash sah teli <155549636+23AD094OP@users.noreply.github.com>
2024-05-20 09:22:26 +0000

    create code

--- question4.py
+++ question4.py
@@ -1,1 +1,9 @@
 #write a program to find the sum of digits of a given number'
+num = int(input("Enter the input count: "))
+i=0
+sum = 0
+while i<num:
+    n = int(input("Enter the number:- "))
+    sum = sum + n
+    i = i+1
+print(sum)    
