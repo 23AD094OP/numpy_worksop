d01befd00b43e901c65ff1bdc3ab3ef6aa98c32a
Om prakash sah teli <155549636+23AD094OP@users.noreply.github.com>
2024-05-20 09:31:26 +0000

    create code

--- question6.py
+++ question6.py
@@ -1,1 +1,7 @@
 #write a program to find the maximum of two numbers
+num = int(input("Enter the first number"))
+num1 = int(input("Enter the second number"))
+if num>num1:
+    print(num,"is maxmium number")
+else:
+    print(num,"is maxmium number")    
